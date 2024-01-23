a64b3771168c7a9c0632bbfb02b8cb577a3f2a98
teddiebui <teddie.bbui@gmail.com>
2024-01-23 19:53:22 +0800

    second commit

--- MyWeb/polls/views.py
+++ MyWeb/polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 # from django.template import loader
-from .models import Question
+from .models import Question, Choice
 from django.http import Http404
 
 
@@ -14,8 +16,8 @@ def index(request):
     # return HttpResponse(template.render(context, request))
     return render(request, "polls/index.html", context)
 
-def detail(request, question_id):
 
+def detail(request, question_id):
     # try:
     #     question = Question.objects.get(pk=question_id)
     # except Question.DoesNotExist:
@@ -24,15 +26,37 @@ def detail(request, question_id):
 
     #### It’s a very common idiom to use get() and raise Http404 if the object doesn’t exist.
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    return render(request, "polls/details.html", {"question": question})
 
-    #There’s also a get_list_or_404() function, which works just as get_object_or_404() – except using filter() instead of get(). It raises Http404 if the list is empty.
+    # There’s also a get_list_or_404() function, which works just as get_object_or_404() – except using filter() instead of get(). It raises Http404 if the list is empty.
 
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
+    me = []
     return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/details.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
     return HttpResponse("You're voting on question %s." % question_id)
